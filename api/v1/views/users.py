47114b3880168a20ef3197d0b470f88764ee7070
Henry <ikegwuonuhenry25@gmail.com>
2023-07-30 23:42:16 +0000

    v3

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,36 +1,75 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import hashlib
+"""
+This file contains the User module
+"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from models import storage
+from models.user import User
+from flasgger.utils import swag_from
 
 
-class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/get.yml', methods=['GET'])
+def get_all_users():
+    """ get users by id"""
+    all_list = [obj.to_dict() for obj in storage.all(User).values()]
+    return jsonify(all_list)
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
 
-    def __setattr__(self, k, v):
-        """sets user pasword"""
-        if k == "password":
-            v = hashlib.md5(v.encode()).hexdigest()
-        super().__setattr__(k, v)
+@app_views.route('/users/<string:user_id>', methods=['GET'],
+                 strict_slashes=False)
+@swag_from('documentation/user/get_id.yml', methods=['GET'])
+def get_user(user_id):
+    """ get user by id"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<string:user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documentation/user/delete.yml', methods=['DELETE'])
+def del_user(user_id):
+    """ delete user by id"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/users/', methods=['POST'],
+                 strict_slashes=False)
+@swag_from('documentation/user/post.yml', methods=['POST'])
+def create_obj_user():
+    """ create new instance """
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if 'email' not in request.get_json():
+        return make_response(jsonify({"error": "Missing email"}), 400)
+    if 'password'not in request.get_json():
+        return make_response(jsonify({"error": "Missing password"}), 400)
+    js = request.get_json()
+    obj = User(**js)
+    obj.save()
+    return (jsonify(obj.to_dict()), 201)
+
+
+@app_views.route('/users/<string:user_id>', methods=['PUT'],
+                 strict_slashes=False)
+@swag_from('documentation/user/put.yml', methods=['PUT'])
+def post_user(user_id):
+    """  """
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    obj = storage.get(User, user_id)
+    if obj is None:
+        abort(404)
+    for key, value in request.get_json().items():
+        if key not in ['id', 'email', 'created_at', 'updated']:
+            setattr(obj, key, value)
+    storage.save()
+    return jsonify(obj.to_dict())
